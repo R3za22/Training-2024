a8430b35f2cb15fadd6c02d9e50f7a680f42905e
R3za22 <rezaghamar20@gmail.com>
2024-03-22 15:03:58 +0330

    finished react 101

---
+++ React/React101/src/components/TabButton.jsx
@@ -0,0 +1,7 @@
+export default function TabButton({children, onSelect, isSelected}) {
+    return (
+    <li>
+      <button className={isSelected?"active":undefined} onClick={onSelect}>{children}</button>
+    </li>
+  );
+}
