140bafc3c45070c7dc3896d9682c8718007270e9
R3za22 <rezaghamar20@gmail.com>
2024-04-06 15:28:50 +0330

    project manager
    relocating project manager as a training proj instead of a repository

---
+++ React/8 - Project Manager/Project-Manager/src/App.jsx
@@ -0,0 +1,128 @@
+import { useState } from "react";
+
+import Newproject from "./components/NewProject";
+import ProjectsSidebar from "./components/ProjectsSidebar";
+import NoProjectSelected from "./components/NoProjectSelected";
+import SelectedProject from "./components/SelectedProject";
+
+function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+    tasks: [],
+  });
+
+  function handleAddTask(text) {
+    setProjectsState((prev) => {
+      const taskId = Math.random();
+      const newTask = {
+        text: text,
+        projectId: prev.selectedProjectId,
+        id: taskId,
+      };
+
+      return {
+        ...prev,
+        tasks: [newTask, ...prev.tasks],
+      };
+    });
+  }
+
+  function handleDeleteTask(id) {
+    setProjectsState((prev) => {
+      return {
+        ...prev,
+        tasks: prev.tasks.filter((task) => task.id !== id),
+      };
+    });
+  }
+
+  function handleSelectproject(id) {
+    setProjectsState((prev) => {
+      return {
+        ...prev,
+        selectedProjectId: id,
+      };
+    });
+  }
+
+  function handleStartAddProject() {
+    setProjectsState((prev) => {
+      return {
+        ...prev,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  function handleCancelAddProject() {
+    setProjectsState((prev) => {
+      return {
+        ...prev,
+        selectedProjectId: undefined,
+      };
+    });
+  }
+
+  function handleAddProject(projectData) {
+    setProjectsState((prev) => {
+      const projectId = Math.random();
+      const newProject = {
+        ...projectData,
+        id: projectId,
+      };
+
+      return {
+        ...prev,
+        selectedProjectId: undefined,
+        projects: [...prev.projects, newProject],
+      };
+    });
+  }
+
+  function handleDeleteProject() {
+    setProjectsState((prev) => {
+      return {
+        ...prev,
+        selectedProjectId: undefined,
+        projects: prev.projects.filter(
+          (project) => project.id !== prev.selectedProjectId
+        ),
+      };
+    });
+  }
+
+  const selectedProject = projectsState.projects.find(
+    (project) => project.id === projectsState.selectedProjectId
+  );
+
+  let content = (
+    <SelectedProject
+      project={selectedProject}
+      onDelete={handleDeleteProject}
+      onAddTask={handleAddTask}
+      onDeleteTask={handleDeleteTask}
+      tasks={projectsState.tasks}
+    />
+  );
+  if (projectsState.selectedProjectId === null) {
+    content = (
+      <Newproject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
+    );
+  } else if (projectsState.selectedProjectId === undefined)
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+
+  return (
+    <main className=' h-screen my-8 flex gap-8'>
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+        onSelectProject={handleSelectproject}
+        selectedProjectId={projectsState.selectedProjectId}
+      />
+      {content}
+    </main>
+  );
+}
+
+export default App;
---
+++ React/8 - Project Manager/Project-Manager/src/components/NewTask.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+export default function NewTasks({ onAdd }) {
+  const [enteredTask, setEnteredTask] = useState("");
+
+  function handleChange(e) {
+    setEnteredTask(e.target.value);
+  }
+
+  function handleClick() {
+    if (enteredTask.trim() === "") {
+      return;
+    }
+    onAdd(enteredTask);
+    setEnteredTask("");
+  }
+
+  return (
+    <div className=' flex items-center gap-4'>
+      <input
+        type='text'
+        className=' w-64 px-2 py-1 rounded-sm bg-stone-200'
+        onChange={handleChange}
+        value={enteredTask}
+      />
+      <button
+        className=' text-stone-700 hover:text-stone-950'
+        onClick={handleClick}
+      >
+        Add Task
+      </button>
+    </div>
+  );
+}
